d585d46a1bdef855c0be034a5a00650fffdaca05
Suyog <karkisuyognp@gmail.com>
2025-09-14 15:40:15 -0500

    Saving changes before switching to main

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,20 +2,24 @@
 import express from "express";
 
 import dotenv from "dotenv"
-
+import cookieParser from "cookie-parser";
 import{connectDB} from "./lib/db.js"
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config () 
  
 const app = express();
+app.use(cookieParser());
 
+app.use(express.json());
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
+
  const PORT = process.env.PORT
 
-
 app.listen(PORT, () => {
-    console.log("server is running on port"+ PORT);
+    console.log("server is running on port "+ PORT);
     connectDB()
 });
